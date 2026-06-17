db70f913eee7b3abe730937f99c613046c4e807b
tadano-hito <168138976+tadano-hito@users.noreply.github.com>
2026-06-17 17:56:49 +0500

    Update day_3.py

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -5,3 +5,15 @@ print(fruits[0:3:2])
 print(fruits[0:4:2])
 print(fruits[0:5:2])
 print(fruits[::-1])
+
+contract={}
+
+for i in range(5):
+    name= input("enter ur name ")
+    number=input("enter ur phone number ")
+    contract[name]= number
+
+for name, number in contract.items():
+    print("contract info")
+    print(f"{name} : {number}")
+
