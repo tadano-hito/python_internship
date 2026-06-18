eea305e8d22cfead0fee50eb91bf7385de87ef27
tadano-hito <168138976+tadano-hito@users.noreply.github.com>
2026-06-18 16:49:27 +0500

    updated day3

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -7,34 +7,34 @@ print(fruits[0:5:2])
 print(fruits[::-1])
 print(fruits[0:1:-1])
 
-# contacts={}
+contacts={}
 
-# for i in range(5):
-#     name= input("enter ur name ")
-#     number=input("enter ur phone number ")
-#     contacts[name]= number
+for i in range(5):
+    name= input("enter ur name ")
+    number=input("enter ur phone number ")
+    contacts[name]= number
 
-# for name, number in contacts.items():
-#     print("contacts info")
-#     print(f"{name} : {number}")
+for name, number in contacts.items():
+    print("contacts info")
+    print(f"{name} : {number}")
 
 
-# name_to_update = input("Enter name to update: ")
-# if name_to_update in contacts:
-#     new_number = input("Enter new number: ")
-#     contacts[name_to_update] = new_number
-#     print("Updated!")
-# else:
-#     print("contacts not found")
+name_to_update = input("Enter name to update: ")
+if name_to_update in contacts:
+    new_number = input("Enter new number: ")
+    contacts[name_to_update] = new_number
+    print("Updated!")
+else:
+    print("contacts not found")
 
-# name_to_delete = input("Enter name to delete: ")
-# if name_to_delete in contacts:
-#     del contacts[name_to_delete]
-#     print("Deleted!")
-# else:
-#     print("Contact not found")
+name_to_delete = input("Enter name to delete: ")
+if name_to_delete in contacts:
+    del contacts[name_to_delete]
+    print("Deleted!")
+else:
+    print("Contact not found")
 
-# print(contacts)
+print(contacts)
 
 s=("hellomf")
 print(s.upper(),
@@ -50,3 +50,11 @@ s.isalnum()
 )
 new_s = s[:5] + " " + s[5:]
 print(new_s)
+
+def summ ():
+    a=int(input("please enter a value: "))
+    b=int(input("please enter a value: "))
+    return a*b
+
+
+print(summ())
