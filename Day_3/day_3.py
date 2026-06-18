aa8cdff44271a350bb083fa7f2d078304feaf3fe
tadano-hito <168138976+tadano-hito@users.noreply.github.com>
2026-06-18 17:46:52 +0500

    Update day_3.py

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -7,34 +7,34 @@ print(fruits[0:5:2])
 print(fruits[::-1])
 print(fruits[0:1:-1])
 
-contacts={}
+# contacts={}
 
-for i in range(5):
-    name= input("enter ur name ")
-    number=input("enter ur phone number ")
-    contacts[name]= number
+# for i in range(5):
+#     name= input("enter ur name ")
+#     number=input("enter ur phone number ")
+#     contacts[name]= number
 
-for name, number in contacts.items():
-    print("contacts info")
-    print(f"{name} : {number}")
+# for name, number in contacts.items():
+#     print("contacts info")
+#     print(f"{name} : {number}")
 
 
-name_to_update = input("Enter name to update: ")
-if name_to_update in contacts:
-    new_number = input("Enter new number: ")
-    contacts[name_to_update] = new_number
-    print("Updated!")
-else:
-    print("contacts not found")
+# name_to_update = input("Enter name to update: ")
+# if name_to_update in contacts:
+#     new_number = input("Enter new number: ")
+#     contacts[name_to_update] = new_number
+#     print("Updated!")
+# else:
+#     print("contacts not found")
 
-name_to_delete = input("Enter name to delete: ")
-if name_to_delete in contacts:
-    del contacts[name_to_delete]
-    print("Deleted!")
-else:
-    print("Contact not found")
+# name_to_delete = input("Enter name to delete: ")
+# if name_to_delete in contacts:
+#     del contacts[name_to_delete]
+#     print("Deleted!")
+# else:
+#     print("Contact not found")
 
-print(contacts)
+# print(contacts)
 
 s=("hellomf")
 print(s.upper(),
@@ -51,10 +51,42 @@ s.isalnum()
 new_s = s[:5] + " " + s[5:]
 print(new_s)
 
-def summ ():
-    a=int(input("please enter a value: "))
-    b=int(input("please enter a value: "))
-    return a*b
+# def summ ():
+#     a=int(input("please enter a value: "))
+#     b=int(input("please enter a value: "))
+#     return a*b
 
 
-print(summ())
+# print(summ())
+
+
+contact = ("Ali", "0300-1234567")
+(name, phone_num)=contact
+print(f"{name}'s number is {phone_num}")
+
+contacts_list = [
+    ("Ali", "0300-1111111"),
+    ("Sara", "0300-2222222"),
+    ("Bilal", "0300-3333333"),
+]
+(name, phone_num)=contact
+for name, phone_num in contacts_list:
+    print(f"{name}'s number is {phone_num}")
+
+
+work_contacts = {"Ali", "Sara", "Bilal", "Hina"}
+family_contacts = {"Sara", "Ahmed", "Bilal", "Zara"}
+
+print(work_contacts | family_contacts)
+print(work_contacts & family_contacts)
+print(work_contacts - family_contacts)
+print(work_contacts ^ family_contacts)
+
+contacts = {
+    "Ali": {"phone": "0300-1111111", "email": "ali@gmail.com"},
+    "Ahmed": {"phone": "0300-2222222", "email": "ahmed@gmail.com"}
+}
+
+print(contacts["Ali"]["email"])
+print(contacts["Ahmed"]["phone"])
+
