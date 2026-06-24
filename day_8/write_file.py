ffcc4380c7a3090bdd10224932ce9a2428817c8d
tadano-hito <168138976+tadano-hito@users.noreply.github.com>
2026-06-24 11:17:08 +0500

    updated

--- day_8/write_file.py
+++ day_8/write_file.py
@@ -1,4 +1,10 @@
-txt_file="hey nigga hwyd!"
-file_path="Good_greeting.txt"
+# txt_file="hey nigga hwyd!"
+# file_path="Good_greeting.txt"
 
-with open(file_path)
+# with open(file_path, "a") as f:
+#     f.write("\n"+txt_file)
+#     print (f"txt file : '{file_path}' was created")
+
+
+def save_to_json():
+    data=[acc.dict]
