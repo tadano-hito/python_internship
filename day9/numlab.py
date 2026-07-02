a84fbfc736d3638cec4cb6d66080bd5c0faf9c9f
tadano-hito <168138976+tadano-hito@users.noreply.github.com>
2026-07-02 17:22:01 +0500

    Update numlab.py

--- day9/numlab.py
+++ day9/numlab.py
@@ -37,3 +37,47 @@ def solve_hungarian(profit: np.ndarray) -> tuple[list[int], int]:
             break
         uncovered_rows = [i for i in range(n) if assigned_rows[i] == -1]
         uncovered_cols = [j for j in range(n) if not used_cols[j]]
+        d = cost[np.ix_(uncovered_rows, uncovered_cols)].min()
+        cost[np.ix_(uncovered_rows, uncovered_cols)] -= d
+        covered_rows = [i for i in range(n) if assigned_rows[i] != -1]
+        covered_cols = [j for j in range(n) if used_cols[j]]
+        cost[np.ix_(covered_rows, covered_cols)] += d
+    total_profit = sum(profit[i][assigned_rows[i]] for i in range(n))
+    return (assigned_rows),total_profit
+
+
+def verify(trials: int = 3, max_n: int = 7) -> None:
+    for t in range(trials):
+        print(f"Trial {t}")
+        n = np.random.randint(2, max_n + 1) 
+        P = np.random.randint(1, 100, size=(n, n))
+        
+        _, total_brute = solve_brute_force(P)
+        _, total_hungarian = solve_hungarian(P)
+        
+        assert total_brute == total_hungarian, f"MISMATCH on n={n}: brute={total_brute}, hungarian={total_hungarian}"
+    
+    print(f"All {trials} trials passed!")
+
+
+# def verify(trials: int = 3, max_n: int = 7) -> None:
+#     for _ in range(trials):
+#         n = np.random.randint(2, max_n + 1) 
+#         P = np.random.randint(1, 100, size=(n, n))
+        
+#         _, total_brute = solve_brute_force(P)
+#         _, total_hungarian = solve_hungarian(P)
+        
+#         assert total_brute == total_hungarian, f"MISMATCH on n={n}: brute={total_brute}, hungarian={total_hungarian}"
+    
+#     print(f"All {trials} trials passed!")
+
+if __name__ == "__main__":
+    P = np.array([
+        [9, 11, 14],
+        [6, 15, 13],
+        [12, 13, 6],
+    ])
+    print(solve_brute_force(P))
+    print(solve_hungarian(P))
+    verify()
